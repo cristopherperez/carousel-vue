104965233928bea2b05b5f86184caaa0723a303b
cristopherperez <cristopherperez88@gmail.com>
2021-02-15 15:04:01 -0300

    carrusel de imagenes hecho con vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <carousel 
+      @next="next"
+      @prev="prev">
+      
+      <carousel-slide v-for="(slide, index) in slides" 
+        :key="slide" 
+        :index="index"
+        :visibleSlide = "visibleSlide"
+        :direction="direction">
+        <img :src="slide" />
+      </carousel-slide>
+    </carousel>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Carousel from './components/Carousel';
+import CarouselSlide from './components/CarouselSlide';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return{
+      slides: [
+        'https://picsum.photos/id/237/600/350',
+        'https://picsum.photos/id/236/600/350',
+        'https://picsum.photos/id/235/600/350',
+        'https://picsum.photos/id/234/600/350',
+        'https://picsum.photos/id/233/600/350',
+        'https://picsum.photos/id/232/600/350',
+      ],
+      visibleSlide: 0,
+      direction: 'left',
+    }
+  },
+  computed: {
+    slidesLen(){
+      return this.slides.length;
+    }
+  },
+  methods :{
+    next(){
+      if(this.visibleSlide >= this.slidesLen - 1){
+        this.visibleSlide = 0;
+      } else{ 
+        this.visibleSlide++;
+      }
+      this.direction = "left"
+    },
+    prev(){
+      if(this.visibleSlide <= 0){
+        this.visibleSlide = this.slidesLen - 1
+      } else{ 
+        this.visibleSlide--;
+      }
+      this.direction = "right"
+
+    }
+  },
+  components: { 
+    Carousel, 
+    CarouselSlide,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .app{
+    display: flex;
+    justify-content: center;
+  }
 </style>
